82a8466beed92858788cd2419fbbf1e514f7528f
danvega <danvega@gmail.com>
2025-02-21 16:16:30 -0500

    Missing annotation on memory controller

--- src/main/java/dev/danvega/confoo/memory/MemoryController.java
+++ src/main/java/dev/danvega/confoo/memory/MemoryController.java
@@ -5,7 +5,9 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MemoryController {
 
     private final ChatClient chatClient;
